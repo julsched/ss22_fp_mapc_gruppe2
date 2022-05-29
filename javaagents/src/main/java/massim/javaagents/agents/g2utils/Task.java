17a009ba80d0412baa0b96065f2cab79c8984f4f
MichaelSch91 <michael_s33@gmx.de>
2022-05-29 14:16:01 +0200

    Erweiterung von Task um blockTypeMap und blockMatrix
    Task wurde erweitert, so dass für jede Task eine HashMap erstellt wird, in der die Art und Anzahl der Blöcke gespeichert wird und eine 5x5 Matrix wird erstellt, in der die Position der Blöcke zum Agenten gespeichert wird (funktioniert derzeit nur, wenn alle Blöcke unter dem Agenten sind)

--- javaagents/src/main/java/massim/javaagents/agents/g2utils/Task.java
+++ javaagents/src/main/java/massim/javaagents/agents/g2utils/Task.java
@@ -8,12 +8,20 @@ public class Task {
     private final int deadline;
     private final int reward;
     private final List<TaskRequirement> requirements;
+    
+    private String[][] blockMatrix;;
+    private HashMap<String, Integer> blockTypeMap;
 
-    public Task(String name, int deadline, int reward, List<TaskRequirement> requirements) {
+    public Task(String name, int deadline, int reward, List<TaskRequirement> requirements) throws Exception {
         this.name = name;
         this.deadline = deadline;
         this.reward = reward;
         this.requirements = requirements;
+        
+        // create blockMatrix to choose where to place blocks
+        this.blockMatrix = createBlockMatrix();
+        // create blockTypeMap to decide which block to get
+		this.blockTypeMap = createBlockTypeMap();
     }
 
     public String getName() {
@@ -39,4 +47,84 @@ public class Task {
             return false;
         }
     }
+    
+	/**
+	 * editor: michael
+	 *
+	 * determine if task needs more than a single block
+	 *
+	 * @return
+	 */
+	public boolean isMultiBlockTask() {
+		if (this.requirements.size() > 1) return true;
+		return false;		
+	}
+	
+
+	/**
+	 * editor: michael
+	 *
+	 * creates BlockMatrix, sets blockName in place of array
+	 * only works if blocks are arranged under agent 
+	 *
+	 * @return
+	 */
+	public String[][] createBlockMatrix(){
+		String[][] matrix = new String[5][5];
+		for (int i =0; i< this.requirements.size(); i++) {
+			matrix
+				[this.requirements.get(i).getRelativeCoordinate().getX()+2]
+				[this.requirements.get(i).getRelativeCoordinate().getY()] 
+						= this.requirements.get(i).getBlockType();
+		}
+		//test
+		for (int i = 0; i < matrix.length; i++) {
+		    for (int j = 0; j < matrix[i].length; j++) {
+		        System.out.print(matrix[i][j] + " ");
+		    }
+		    System.out.println();
+		}
+		return matrix;		
+	}
+	
+	/**
+	 * editor: michael
+	 * 
+	 * creates HashMap to connect blockName and amount needed for task
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	public HashMap<String, Integer> createBlockTypeMap() throws Exception{
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("b0",0);
+		map.put("b1",0);
+		map.put("b2",0);
+		map.put("b3",0);
+		
+		for (int i =0; i< this.requirements.size(); i++) {			
+			switch(this.requirements.get(i).getBlockType()) {
+			case "b0":
+				map.put("b0", map.get("b0")+1);
+			case "b1":
+				map.put("b1", map.get("b1")+1);
+			case "b2":
+				map.put("b2", map.get("b2")+1);
+			case "b3":
+				map.put("b3", map.get("b3")+1);
+			default:
+				throw new Exception("setBlockTypeMap cannot handle: "+this.requirements.get(i).getBlockType());
+			}
+		}
+		return map;
+	}
+
+	public String[][] getBlockMatrix() {
+		return blockMatrix;
+	}
+
+	public HashMap<String, Integer> getBlockTypeMap() {
+		return blockTypeMap;
+	}
+	
 }
